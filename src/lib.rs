3be59dd3f6b772e6e7732fc8b8708ecbb0e8e482
R4chel <rae84@cornell.edu>
2021-02-21 14:34:48 -0500

    stop saying hi

--- src/lib.rs
+++ src/lib.rs
@@ -69,18 +69,6 @@ fn request_animation_frame(f: &Closure<dyn FnMut()>) {
 // Called when the wasm module is instantiated
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
     let mut universe = Universe {
         config: Config {
             height: 250.0,
